f48ab89b95d7e7f737125465dd43897a3b246b0b
Sergio-Lino-Pereira <auditoriasergio@hotmail.com>
2023-02-22 11:04:48 -0400

    códigos var e conversões

---
+++ script.js
@@ -0,0 +1,45 @@
+// var
+// var altura = 5;
+// var comprimento = 7;
+// var area = altura * comprimento;
+// console.log("var: "+area)
+
+// let
+// let forma = 'retângulo'; 
+// let altura2 = 5;
+// let comprimento2 = 7;
+// let area2;
+// area2 = altura2*comprimento2;
+
+// if(forma === 'retângulo'){
+//     area2 = altura2*comprimento2;
+// }else{ 
+//     area2 = (altura2*comprimento2) / 2;
+// }
+// console.log("let: "+area2)    
+
+// const
+
+const forma = 'outra';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+ if (forma === 'retângulo'){
+    area = altura * comprimento;
+ }else{
+    area = (altura * comprimento) / 2;
+ }
+
+console.log(area)
+
+
+
+
+
+
+
+
+
+
+
---
+++ truthyfalsy.js
@@ -0,0 +1,14 @@
+// boolean
+true
+false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+let numero = 3;
+let texto = "estudo";
+
+console.log(typeof numero)
+console.log(typeof texto)
